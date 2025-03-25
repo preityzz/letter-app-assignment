219694a05a4cb160524f76cec543d3d3f80a61b0
preityzz <106267402+preityzz@users.noreply.github.com>
2025-03-25 23:49:51 +0530

    added editor page

---
+++ app/(main)/editor/[documentId]/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import RichTextEditor from "@/components/editor/RichTextEditor";
+
+export default function Page() {
+  return <RichTextEditor />;
+}
---
+++ components/editor/RichTextEditor.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function RichTextEditor() {
+  return <div>Rich Text Editor</div>;
+}
